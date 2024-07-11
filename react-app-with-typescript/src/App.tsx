85c279626c6569447e4b734827fdd8b050b57cf9
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-07-11 13:06:45 -0400

    vista postulante

--- react-app-with-typescript/src/App.tsx
+++ react-app-with-typescript/src/App.tsx
@@ -9,7 +9,11 @@ function App() {
       nick: "Alex M",
       quizLevel: "Junior",
       percentage: 80,
-      description: "Soy un programador novato"
+      email: "alex@mail.cl",
+      description: "Soy un programador novato",
+      studies: "Informatica UDP",
+      avatar: "https://i.pravatar.cc/150?u=alexml",
+      experience:"none"
     }
   ]);
 
@@ -26,6 +30,27 @@ function App() {
       percentage: 70,
       slogan: "Looking for good employees"
     }
+    ,
+    {
+      comapanyName: "Apple",
+      quizLevel: "Junior",
+      percentage: 75,
+      slogan: "Looking for good employees"
+    }
+    ,
+    {
+      comapanyName: "Amazon",
+      quizLevel: "Junior",
+      percentage: 70,
+      slogan: "Looking for good employees"
+    }
+    ,
+    {
+      comapanyName: "Funeraria Ivan Martínez",
+      quizLevel: "Novato",
+      percentage: 60,
+      slogan: "Buscando buenos empleados"
+    }
   ]);
 
 
@@ -34,18 +59,30 @@ function App() {
     <div className="App">
       <img src='https://talenfly.com/wp-content/uploads/2021/12/cropped-Mesa-de-trabajo-%e2%80%93-2-180x180.jpg' className='logoTF'></img>
       {
-      user.map(u => {
-        return(
-          <h1 className='colorTF'>Welcome {u.nick}</h1>
-          
-        )
-      })
+        user.map(u => {
+          return(
+            <div>
+              <h1 className='colorTF'>Welcome {u.nick}</h1>
+              <form className='userCard'>
+                <img src={u.avatar}></img>
+                <h3>{u.nick}</h3>
+                <p className='userinfo'>Correo: {u.email}</p>
+                <p className='userinfo'>Perfil profesional: {u.studies}</p>
+                <p className='userinfo'>Experiencia laboral: {u.experience}</p>
+                <p className='userinfo'>{u.description}</p>
+                <div className='userlevel'>
+                  <h2> Nivel: {u.quizLevel} con {u.percentage}% de aprobación</h2>
+                </div>
+              </form>
+            </div>
+          )
+        })
       }
-      <ul>
+      <ul className='grid-container'>
         {
           company.map((comp, index) => {
             return (
-              <form key={index}>
+              <form key={comp.comapanyName}>
                 <h2>{comp.comapanyName}</h2>
                 <p>{comp.slogan}</p>
                 <p>Looking for: {comp.percentage}% {comp.quizLevel}</p>
